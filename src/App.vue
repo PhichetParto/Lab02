b28ce418cb50563cf9bce3e2020eadf84270c9dc
Phichet Parto <64160261@go.buu.ac.th>
2022-11-28 23:44:26 +0700

    Handing User Input

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@ import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import CalComponentVue from "./components/CalComponent.vue";
 import HelloWordExample from "./components/HelloWordExample.vue";
+import HandingUserInput from "./components/HandingUserInput.vue";
 </script>
 
 <template>
   <div>
+    <HandingUserInput></HandingUserInput>
     <CalComponentVue></CalComponentVue>
     <HelloWordExample></HelloWordExample>
   </div>
