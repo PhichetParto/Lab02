a0ff58a4b16147b0a08b396f24e771a9b4b05bc3
Phichet Parto <64160261@go.buu.ac.th>
2022-11-29 00:15:44 +0700

    Attribute Bindings

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@ import HelloWorld from "./components/HelloWorld.vue";
 import CalComponentVue from "./components/CalComponent.vue";
 import HelloWordExample from "./components/HelloWordExample.vue";
 import HandingUserInput from "./components/HandingUserInput.vue";
+import AttributeBindings from "./components/AttributeBindings.vue";
 </script>
 
 <template>
   <div>
+    <AttributeBindings></AttributeBindings>
     <HandingUserInput></HandingUserInput>
     <CalComponentVue></CalComponentVue>
     <HelloWordExample></HelloWordExample>
