46ec2d870630c15f5624144d5dacdd20399ef9ba
Phichet Parto <64160261@go.buu.ac.th>
2022-11-28 22:52:47 +0700

    Hello World Example

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import CalComponentVue from "./components/CalComponent.vue";
+import HelloWordExample from "./components/HelloWordExample.vue";
 </script>
 
 <template>
   <div>
     <CalComponentVue></CalComponentVue>
+    <HelloWordExample></HelloWordExample>
   </div>
 </template>
 
