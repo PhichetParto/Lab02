7e55b588fee4b0bd992877d461674ef18c8195f3
Phichet Parto <64160261@go.buu.ac.th>
2022-11-29 00:39:34 +0700

    Conditionals Bindings

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,14 @@ import CalComponentVue from "./components/CalComponent.vue";
 import HelloWordExample from "./components/HelloWordExample.vue";
 import HandingUserInput from "./components/HandingUserInput.vue";
 import AttributeBindings from "./components/AttributeBindings.vue";
+import ConditionsAndLoops from "./components/ConditionAndLoops.vue";
 </script>
 
 <template>
   <div>
+    <ConditionsAndLoops></ConditionsAndLoops>
+    <br />
+    <br />
     <AttributeBindings></AttributeBindings>
     <HandingUserInput></HandingUserInput>
     <CalComponentVue></CalComponentVue>
---
+++ src/components/ConditionAndLoops.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const show = ref(true);
+const list = ref([1, 2, 3]);
+</script>
+
+<template>
+  <button @click="show = !show">Toggle List</button>
+  <button @click="list.push(list.length + 1)">Push Number</button>
+  <button @click="list.pop()">Pop Number</button>
+  <button @click="list = list.reverse()">Reverse List</button>
+
+  <u1 v-if="show && list.length">
+    <li v-for="(item, index) of list" :key="index">{{ item }}</li>
+  </u1>
+  <p v-else-if="list.length">List is not empty, but hidden.</p>
+  <p v-else>List is empty</p>
+</template>
+
+<style scoped></style>
