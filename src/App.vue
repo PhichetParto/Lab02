2377b825629436e620e483470caa9be395424eea
Phichet Parto <64160261@go.buu.ac.th>
2022-11-29 09:11:38 +0700

    Simple Component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
-import CalComponentVue from "./components/CalComponent.vue";
+import CalComponent from "./components/CalComponent.vue";
 import HelloWordExample from "./components/HelloWordExample.vue";
 import HandingUserInput from "./components/HandingUserInput.vue";
 import AttributeBindings from "./components/AttributeBindings.vue";
 import ConditionsAndLoops from "./components/ConditionAndLoops.vue";
 import FormBinding from "./components/FormBinding.vue";
+import SimPleComponent from "./components/SimPleComponent.vue";
 </script>
 
 <template>
   <div>
+    <SimPleComponent></SimPleComponent>
+    <br />
+    <br />
     <FormBinding></FormBinding>
     <br />
     <br />
---
+++ src/components/SimPleComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import TodoItem from "./TodoItem.vue";
+const groceryList = ref([
+  { id: 0, text: "Vegetables" },
+  { id: 1, text: "Cheese" },
+  { id: 2, text: "Whatever else humans are supposed to eat" },
+]);
+</script>
+
+<template>
+  <ol>
+    <TodoItem v-for="item of groceryList" :key="item.id" :todo="item" />
+  </ol>
+</template>
